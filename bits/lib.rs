18c9eefa7bfdf5fb02c81de27b18a2e073b67609
Kohei Watanabe <mypmc@symplectik.com>
2022-05-18 01:50:05 +0900

    cleanup modules

--- bits/lib.rs
+++ bits/lib.rs
@@ -7,14 +7,21 @@ mod bit_len;
 mod bit_put;
 mod bit_rank;
 mod bit_select;
-pub mod ops;
-
-mod bits;
 mod word;
 
 mod bools;
 mod impls;
 mod slice;
+
+pub mod ops {
+    pub use crate::bit_count::BitCount;
+    pub use crate::bit_excess::BitExcess;
+    pub use crate::bit_get::BitGet;
+    pub use crate::bit_len::BitLen;
+    pub use crate::bit_put::BitPut;
+    pub use crate::bit_rank::BitRank;
+    pub use crate::bit_select::BitSelect;
+}
 
 pub use crate::bits::*;
 pub use crate::word::Word;
@@ -44,7 +51,8 @@ fn address<T: Bits>(i: usize) -> (usize, usize) {
     divrem(i, T::BITS)
 }
 
-/// A utility to clamp a given range within a valid range.
+/// A utility to clamp the given range into a valid one.
+/// Panics if debug is enabled and `min <= i && i <= j && j <= max`.
 fn to_range<R: core::ops::RangeBounds<usize>>(r: &R, min: usize, max: usize) -> (usize, usize) {
     use core::ops::Bound::*;
 
@@ -88,3 +96,159 @@ pub fn with_capacity<T: Bits>(n: usize) -> Vec<T> {
 //     let size = blocks(n, T::BITS);
 //     from_fn(|| Some(T::empty())).take(size).collect()
 // }
+
+mod bits {
+    use crate::{ops::*, Word};
+    use core::ops::RangeBounds;
+
+    #[inline]
+    pub fn len<T>(bits: &T) -> usize
+    where
+        T: ?Sized + BitLen,
+    {
+        BitLen::len(bits)
+    }
+
+    #[inline]
+    pub fn is_empty<T>(bits: &T) -> bool
+    where
+        T: ?Sized + BitLen,
+    {
+        BitLen::is_empty(bits)
+    }
+
+    #[inline]
+    pub fn get<T>(bits: &T, i: usize) -> Option<bool>
+    where
+        T: ?Sized + BitGet,
+    {
+        BitGet::get(bits, i)
+    }
+
+    #[inline]
+    pub fn test<T>(bits: &T, i: usize) -> bool
+    where
+        T: ?Sized + BitGet,
+    {
+        BitGet::test(bits, i)
+    }
+
+    #[inline]
+    pub fn put_1<T>(bits: &mut T, i: usize)
+    where
+        T: ?Sized + BitPut,
+    {
+        BitPut::put_1(bits, i)
+    }
+
+    #[inline]
+    pub fn put_0<T>(bits: &mut T, i: usize)
+    where
+        T: ?Sized + BitPut,
+    {
+        BitPut::put_0(bits, i)
+    }
+
+    #[inline]
+    pub fn count_1<T>(bits: &T) -> usize
+    where
+        T: ?Sized + BitCount,
+    {
+        BitCount::count_1(bits)
+    }
+
+    #[inline]
+    pub fn count_0<T>(bits: &T) -> usize
+    where
+        T: ?Sized + BitCount,
+    {
+        BitCount::count_0(bits)
+    }
+
+    #[inline]
+    pub fn all<T>(bits: &T) -> bool
+    where
+        T: ?Sized + BitCount,
+    {
+        BitCount::all(bits)
+    }
+
+    #[inline]
+    pub fn any<T>(bits: &T) -> bool
+    where
+        T: ?Sized + BitCount,
+    {
+        BitCount::any(bits)
+    }
+
+    #[inline]
+    pub fn rank_1<T, Index>(bits: &T, index: Index) -> usize
+    where
+        T: ?Sized + BitRank,
+        Index: RangeBounds<usize>,
+    {
+        BitRank::rank_1(bits, index)
+    }
+
+    #[inline]
+    pub fn rank_0<T, Index>(bits: &T, index: Index) -> usize
+    where
+        T: ?Sized + BitRank,
+        Index: RangeBounds<usize>,
+    {
+        BitRank::rank_0(bits, index)
+    }
+
+    #[inline]
+    pub fn excess_1<T, Index>(bits: &T, index: Index) -> usize
+    where
+        T: ?Sized + BitExcess,
+        Index: RangeBounds<usize>,
+    {
+        BitExcess::excess_1(bits, index)
+    }
+
+    #[inline]
+    pub fn excess_0<T, Index>(bits: &T, index: Index) -> usize
+    where
+        T: ?Sized + BitExcess,
+        Index: RangeBounds<usize>,
+    {
+        BitExcess::excess_0(bits, index)
+    }
+
+    #[inline]
+    pub fn select_1<T>(bits: &T, n: usize) -> Option<usize>
+    where
+        T: ?Sized + BitSelect,
+    {
+        BitSelect::select_1(bits, n)
+    }
+
+    #[inline]
+    pub fn select_0<T>(bits: &T, n: usize) -> Option<usize>
+    where
+        T: ?Sized + BitSelect,
+    {
+        BitSelect::select_0(bits, n)
+    }
+
+    #[doc(hidden)]
+    #[inline]
+    pub fn word<T, U>(bits: &T, i: usize, n: usize) -> U
+    where
+        T: ?Sized + BitGet,
+        U: Word,
+    {
+        BitGet::word(bits, i, n)
+    }
+
+    #[doc(hidden)]
+    #[inline]
+    pub fn put_n<T, N: Word>(bits: &mut T, i: usize, n: usize, mask: N)
+    where
+        T: ?Sized + BitPut,
+    {
+        BitPut::put_n(bits, i, n, mask)
+    }
+}
