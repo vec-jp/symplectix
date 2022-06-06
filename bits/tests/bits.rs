17d775299002177f1759cf36f13bd5ece9e59ace
Kohei Watanabe <mypmc@symplectik.com>
2022-06-07 04:08:40 +0900

    bits: Auxiliary data to help Rank and Select (#18)
    WIP

--- bits/tests/bits.rs
+++ bits/tests/bits.rs
@@ -102,3 +102,36 @@ fn bit_rank() {
     rank_0_plus_rank_1::<[u8]>(&[!0, 0b_1010_1010, !0, 0b_1010_1010], 0..10);
     rank_0_plus_rank_1::<[u8]>(&[!0, 0b_1010_1010, !0, 0b_1010_1010], 7..20);
 }
+
+// #[test]
+// fn aux() {
+//     let mut bv = bitvec::Rho::new((1 << 32) + 10_000);
+//     bv.set_bit(1 << 32);
+//     assert!(bv.bit(1 << 32).unwrap_or_default());
+//     bv.unset_bit(1 << 32);
+//     assert!(!bv.bit(1 << 32).unwrap_or_default());
+// }
+
+#[quickcheck]
+fn repr_bits(vec: Vec<u32>) -> bool {
+    let rho = bitvec::Rho::from(&vec[..]);
+    vec.bits() == rho.bits()
+}
+
+#[quickcheck]
+fn repr_rank(vec: Vec<u32>) -> bool {
+    let rho = bitvec::Rho::from(&vec[..]);
+    vec.count1() == rho.count1() && vec.count0() == rho.count0()
+}
+
+#[quickcheck]
+fn repr_select1(vec: Vec<u32>) -> bool {
+    let rho = bitvec::Rho::from(&vec[..]);
+    (0..vec.count1()).all(|i| vec.select1(i) == rho.select1(i))
+}
+
+#[quickcheck]
+fn repr_select0(vec: Vec<u32>) -> bool {
+    let rho = bitvec::Rho::from(&vec[..]);
+    (0..vec.count0()).all(|i| vec.select0(i) == rho.select0(i))
+}
