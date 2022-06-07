56d89f844deeafe268688d14deaaf589a0b131ad
Kohei Watanabe <mypmc@vec.jp>
2022-06-08 03:11:47 +0900

    devcontainer: obsolete (#21)

--- .devcontainer/workspace/scripts/post_container_create/00_volume_permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-shopt -s inherit_errexit
-
-sudo chown nonroot:nonroot ~/.cache ~/.config ~/.local/share
--- .devcontainer/workspace/scripts/post_image_build/00_locales.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-shopt -s inherit_errexit
-
-sudo sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sudo locale-gen
