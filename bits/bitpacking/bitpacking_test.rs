28a0fcf128f8375885c8e3ebfd937acfff5cda63
Kohei Watanabe <mypmc@symplectik.com>
2022-06-18 23:00:29 +0900

    bits/bitpacking: wip

---
+++ bits/bitpacking/bitpacking_test.rs
@@ -0,0 +1,16 @@
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
+use bitpacking::{Pack, Unpack};
+use bits::Container;
+
+#[quickcheck]
+fn packing_unpacked_bits(mut data: Vec<u32>) -> bool {
+    let orig = data.clone();
+    let len = 7;
+    (0..data.bits()).all(|n| {
+        data.pack(n, len, data.unpack::<u8>(n, len));
+        data.pack(n, len, data.unpack::<i8>(n, len));
+        orig == data
+    })
+}
