562e36f6ae5de1e0c15f96b3a8d48d8a5ed4f927
Kohei Watanabe <mypmc@symplectik.com>
2023-01-11 15:43:19 +0900

    tweaks

--- tool/audit/audit.sh
+++ tool/audit/audit.sh
@@ -2,6 +2,6 @@
 set -euo pipefail -o posix
 
 CARGO_AUDIT_BIN="${CARGO_AUDIT_BIN:-cargo-audit}"
-CARGO_LOCK_FILE="${1:?Path to the target to be audited}"
+CARGO_LOCK_FILE="${1:?Path to the lock file to be audited}"
 
 "${CARGO_AUDIT_BIN}" audit --file "${CARGO_LOCK_FILE}"
