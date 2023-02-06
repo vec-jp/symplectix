b416f7d3412f508f8f06ff395511ffaa8562eb56
Kohei Watanabe <mypmc@symplectik.com>
2023-02-07 03:26:22 +0900

    bits/bitaux: make some progress (#74)

--- bits/bitaux/l1l2.rs
+++ bits/bitaux/l1l2.rs
@@ -3,16 +3,7 @@ use std::fmt::{self, Debug, Formatter};
 use std::iter::Sum;
 use std::ops::{Add, AddAssign, Sub, SubAssign};
 
-/// Interleaves L1[i] and L2[i] into a 64bit unsigned integer.
-#[derive(Copy, Clone, Default, PartialEq, Eq)]
-pub(crate) struct L1L2(pub(crate) u64);
-
-// impl Default for L1L2 {
-//     #[inline]
-//     fn default() -> L1L2 {
-//         L1L2(0)
-//     }
-// }
+use super::*;
 
 impl Debug for L1L2 {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -22,6 +13,72 @@ impl Debug for L1L2 {
             .field(&self.l2_1())
             .field(&self.l2_2())
             .finish()
+    }
+}
+
+impl L1L2 {
+    pub const LEN: usize = 4;
+
+    pub const L1: u64 = 0x_FFFF_FFFF; // lowest 32 bits
+    pub const L2: u64 = 0x_03FF; // lowest 10 bits
+
+    const L2_0_SHIFT: u64 = 32;
+    const L2_1_SHIFT: u64 = 42;
+    const L2_2_SHIFT: u64 = 52;
+
+    #[inline]
+    pub(crate) const fn merge(mut arr: [u64; Self::LEN]) -> Self {
+        debug_assert!(arr[0] < Self::L1);
+        debug_assert!(arr[1] < 1024 && arr[2] < 1024 && arr[3] < 1024);
+
+        arr[0] |= arr[1] << Self::L2_0_SHIFT;
+        arr[0] |= arr[2] << Self::L2_1_SHIFT;
+        arr[0] |= arr[3] << Self::L2_2_SHIFT;
+        L1L2(arr[0])
+    }
+
+    #[inline]
+    pub(crate) const fn split(ll: Self) -> [u64; Self::LEN] {
+        [ll.l1(), ll.l2_0(), ll.l2_1(), ll.l2_2()]
+    }
+
+    #[inline]
+    pub(crate) const fn l1(self) -> u64 {
+        let L1L2(l1l2) = self;
+        l1l2 & Self::L1
+    }
+
+    #[inline]
+    pub(crate) const fn l2_0(self) -> u64 {
+        let L1L2(l1l2) = self;
+        // (l1l2 & Self::MASK_L2_0) >> Self::L2_0_SHIFT
+        (l1l2 >> Self::L2_0_SHIFT) & Self::L2
+    }
+
+    #[inline]
+    pub(crate) const fn l2_1(self) -> u64 {
+        let L1L2(l1l2) = self;
+        // (l1l2 & Self::MASK_L2_1) >> Self::L2_1_SHIFT
+        (l1l2 >> Self::L2_1_SHIFT) & Self::L2
+    }
+
+    #[inline]
+    pub(crate) const fn l2_2(self) -> u64 {
+        let L1L2(l1l2) = self;
+        // (l1l2 & Self::MASK_L2_2) >> Self::L2_2_SHIFT
+        (l1l2 >> Self::L2_2_SHIFT) & Self::L2
+    }
+
+    // Sum of basic blocks. [0,i)
+    #[inline]
+    pub(crate) fn l2(self, i: usize) -> u64 {
+        match i {
+            0 => 0,
+            1 => self.l2_0(),
+            2 => self.l2_0() + self.l2_1(),
+            3 => self.l2_0() + self.l2_1() + self.l2_2(),
+            _ => unreachable!("basic block: index out of bounds"),
+        }
     }
 }
 
@@ -128,73 +185,5 @@ impl SubAssign<L1L2> for u64 {
     #[inline]
     fn sub_assign(&mut self, delta: L1L2) {
         *self -= delta.l1();
-    }
-}
-
-impl L1L2 {
-    pub(crate) const LEN: usize = 4;
-
-    pub(crate) const L1: u64 = 0b_1111_1111_1111_1111_1111_1111_1111_1111; // lowest 32 bits
-    pub(crate) const L2: u64 = 0b_0000_0000_0000_0000_0000_0011_1111_1111;
-    pub(crate) const L2_0_SHIFT: u64 = 32;
-    pub(crate) const L2_1_SHIFT: u64 = 42;
-    pub(crate) const L2_2_SHIFT: u64 = 52;
-
-    #[inline]
-    pub(crate) const fn merge(mut arr: [u64; Self::LEN]) -> Self {
-        #[cfg(test)]
-        {
-            assert!(arr[0] < Self::L1);
-            assert!(arr[1] < 1024 && arr[2] < 1024 && arr[3] < 1024);
-        }
-
-        arr[0] |= arr[1] << Self::L2_0_SHIFT;
-        arr[0] |= arr[2] << Self::L2_1_SHIFT;
-        arr[0] |= arr[3] << Self::L2_2_SHIFT;
-        L1L2(arr[0])
-    }
-
-    #[inline]
-    pub(crate) const fn split(ll: Self) -> [u64; Self::LEN] {
-        [ll.l1(), ll.l2_0(), ll.l2_1(), ll.l2_2()]
-    }
-
-    #[inline]
-    pub(crate) const fn l1(self) -> u64 {
-        let L1L2(l1l2) = self;
-        l1l2 & Self::L1
-    }
-
-    #[inline]
-    pub(crate) const fn l2_0(self) -> u64 {
-        let L1L2(l1l2) = self;
-        // (l1l2 & Self::MASK_L2_0) >> Self::L2_0_SHIFT
-        (l1l2 >> Self::L2_0_SHIFT) & Self::L2
-    }
-
-    #[inline]
-    pub(crate) const fn l2_1(self) -> u64 {
-        let L1L2(l1l2) = self;
-        // (l1l2 & Self::MASK_L2_1) >> Self::L2_1_SHIFT
-        (l1l2 >> Self::L2_1_SHIFT) & Self::L2
-    }
-
-    #[inline]
-    pub(crate) const fn l2_2(self) -> u64 {
-        let L1L2(l1l2) = self;
-        // (l1l2 & Self::MASK_L2_2) >> Self::L2_2_SHIFT
-        (l1l2 >> Self::L2_2_SHIFT) & Self::L2
-    }
-
-    // Sum of basic blocks. [0,i)
-    #[inline]
-    pub(crate) fn l2(self, i: usize) -> u64 {
-        match i {
-            0 => 0,
-            1 => self.l2_0(),
-            2 => self.l2_0() + self.l2_1(),
-            3 => self.l2_0() + self.l2_1() + self.l2_2(),
-            _ => unreachable!("basic block: index out of bounds"),
-        }
     }
 }
