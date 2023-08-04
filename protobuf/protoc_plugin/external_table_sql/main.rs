5aa5173a5a5c99fd3ac507ce8517982fe03adb30
Kohei Watanabe <mypmc@symplectik.com>
2023-08-05 00:49:55 +0900

    WIP; cleanup

--- protobuf/protoc_plugin/external_table_sql/main.rs
+++ protobuf/protoc_plugin/external_table_sql/main.rs
@@ -1,11 +1,4 @@
-use std::collections::HashMap;
-
-use prost_reflect::DescriptorPool;
-use prost_types::{
-    compiler::{code_generator_response, CodeGeneratorRequest, CodeGeneratorResponse},
-    field_descriptor_proto::{Label, Type},
-    FileDescriptorProto, FileDescriptorSet,
-};
+use prost_types::compiler::{code_generator_response, CodeGeneratorRequest, CodeGeneratorResponse};
 
 fn main() -> anyhow::Result<()> {
     protoc_plugin::run(ExternalTableSqlGenerator::default())
@@ -16,36 +9,28 @@ struct ExternalTableSqlGenerator {}
 
 impl protoc_plugin::CodeGenerator for ExternalTableSqlGenerator {
     fn gen_code(&self, req: CodeGeneratorRequest) -> CodeGeneratorResponse {
-        let pool = {
-            let fd_set = FileDescriptorSet { file: req.proto_file.clone() };
-            DescriptorPool::from_file_descriptor_set(fd_set)
-                .expect("failed to create descriptor pool")
-        };
+        let pool = protoc_plugin::create_descriptor_pool(&req);
 
         let file = req
             .file_to_generate
             .iter()
             .map(|file| {
+                let file_name = {
+                    let stem = file.strip_suffix(".proto").expect("no .proto suffix");
+                    format!("{}.external_table.sql", stem)
+                };
+
                 let file_desc = pool
                     .get_file_by_name(file.as_str())
                     .expect("failed to fetch a file descriptor from a pool");
 
                 let mut buf = String::with_capacity(1 << 10);
                 for msg_desc in file_desc.messages() {
-                    // buf.push_str(&format!("{:#?}\n", msg_desc));
-
-                    for field_desc in msg_desc.fields() {
-                        buf.push_str(&format!(
-                            "{name}: {kind:?}\n",
-                            name = field_desc.name(),
-                            kind = field_desc.kind()
-                        ));
-                    }
+                    buf.push_str(&format!("{:#?}\n", msg_desc));
                 }
 
-                let stem = file.strip_suffix(".proto").expect("no .proto suffix");
                 code_generator_response::File {
-                    name: Some(format!("{}.external_table.sql", stem)),
+                    name: Some(file_name),
                     content: Some(buf),
                     ..Default::default()
                 }
