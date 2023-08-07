ba7f5bd2fe102aa132a768c94f2274fa97add1b6
Kohei Watanabe <mypmc@symplectik.com>
2023-08-08 01:15:24 +0900

    impl gen_file

--- protobuf/protoc_plugin/message_descriptor_dump_generator.rs
+++ protobuf/protoc_plugin/message_descriptor_dump_generator.rs
@@ -1,4 +1,5 @@
-use prost_types::compiler::{code_generator_response, CodeGeneratorRequest, CodeGeneratorResponse};
+use prost_reflect::FileDescriptor;
+use prost_types::compiler::code_generator_response as codegen_response;
 
 fn main() -> anyhow::Result<()> {
     protoc_plugin::run(MessageDescriptorDumpGenerator::default())
@@ -7,36 +8,28 @@ fn main() -> anyhow::Result<()> {
 #[derive(Debug, Default, Clone)]
 struct MessageDescriptorDumpGenerator {}
 
-impl protoc_plugin::CodeGenerator for MessageDescriptorDumpGenerator {
-    fn gen_code(&self, req: CodeGeneratorRequest) -> CodeGeneratorResponse {
-        let pool = protoc_plugin::create_descriptor_pool(&req);
+impl protoc_plugin::FileGenerator for MessageDescriptorDumpGenerator {
+    fn gen_file(
+        &self,
+        target_proto: &String,
+        file_desc: &FileDescriptor,
+    ) -> Result<codegen_response::File, String> {
+        let file_name = {
+            let stem = target_proto
+                .strip_suffix(".proto")
+                .ok_or_else(|| format!("unexpected proto '{}'", target_proto))?;
+            format!("{}.message_descriptor_dump", stem)
+        };
 
-        let file = req
-            .file_to_generate
-            .iter()
-            .map(|file| {
-                let file_name = {
-                    let stem = file.strip_suffix(".proto").expect("no .proto suffix");
-                    format!("{}.message_descriptor_dump", stem)
-                };
+        let mut buf = String::with_capacity(1 << 10);
+        for msg_desc in file_desc.messages() {
+            buf.push_str(&format!("{:#?}\n", msg_desc));
+        }
 
-                let file_desc = pool
-                    .get_file_by_name(file.as_str())
-                    .expect("failed to fetch a file descriptor from a pool");
-
-                let mut buf = String::with_capacity(1 << 10);
-                for msg_desc in file_desc.messages() {
-                    buf.push_str(&format!("{:#?}\n", msg_desc));
-                }
-
-                code_generator_response::File {
-                    name: Some(file_name),
-                    content: Some(buf),
-                    ..Default::default()
-                }
-            })
-            .collect();
-
-        CodeGeneratorResponse { file, ..Default::default() }
+        Ok(codegen_response::File {
+            name: Some(file_name),
+            content: Some(buf),
+            ..Default::default()
+        })
     }
 }
