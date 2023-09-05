119a43ba84fe0cd334c0025e6e2874ea4cc61153
Kohei Watanabe <mypmc@symplectik.com>
2023-09-06 01:21:35 +0900

    labels

--- build/stamp/workspace_status.sh
+++ build/stamp/workspace_status.sh
@@ -7,12 +7,12 @@ echo "STABLE_GIT_COMMIT $(git rev-parse HEAD 2>/dev/null)"
 #
 # A unique number for each workflow run within a repository.
 # This number does not change if you re-run the workflow run.
-echo "GITHUB_RUN_ID ${GITHUB_RUN_ID:=0}"
+echo "STABLE_GITHUB_RUN_ID ${GITHUB_RUN_ID:=0}"
 
 # A unique number for each run of a particular workflow in a repository.
 # This number begins at 1 for the workflow's first run, and increments with each new run.
 # This number does not change if you re-run the workflow run.
-echo "GITHUB_RUN_NUMBER ${GITHUB_RUN_NUMBER:=0}"
+echo "STABLE_GITHUB_RUN_NUMBER ${GITHUB_RUN_NUMBER:=0}"
 
 # A unique number for each attempt of a particular workflow run in a repository.
 # This number begins at 1 for the workflow run's first attempt, and increments with each re-run.
