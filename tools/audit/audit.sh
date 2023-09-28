9b6227b1238123970fb418cca5f2a226dd103e45
Kohei Watanabe <mypmc@symplectik.com>
2023-09-28 20:04:14 +0900

    lib_crates to crates

--- tools/audit/audit.sh
+++ tools/audit/audit.sh
@@ -3,10 +3,10 @@ set -euo pipefail -o posix
 
 CARGO_AUDIT_BIN="${CARGO_AUDIT_BIN:-cargo-audit}"
 
-BIN_CRATES_LOCK_FILE="${1:?Path to the bin_crates lock file}"
-LIB_CRATES_LOCK_FILE="${2:?Path to the lib_crates lock file}"
+LIB_CRATES_LOCK_FILE="${1:?Path to the crates lock file}"
+BIN_CRATES_LOCK_FILE="${2:?Path to the bin_crates lock file}"
 shift 2
 
-"${CARGO_AUDIT_BIN}" audit --file "${BIN_CRATES_LOCK_FILE}" --db ./advisory-db "$@" && \
 "${CARGO_AUDIT_BIN}" audit --file "${LIB_CRATES_LOCK_FILE}" --db ./advisory-db "$@" && \
+"${CARGO_AUDIT_BIN}" audit --file "${BIN_CRATES_LOCK_FILE}" --db ./advisory-db "$@" && \
 echo OK
