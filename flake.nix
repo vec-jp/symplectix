37f37fb3cd8bee58670ed9b0430c18373f573367
Kohei Watanabe <mypmc@symplectik.com>
2024-01-06 16:50:16 +0900

    add rust-overlays

--- flake.nix
+++ flake.nix
@@ -14,23 +14,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
+          overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
-            inherit system;
+            inherit system overlays;
           };
         in
         {
           devShells.default = pkgs.mkShellNoCC {
-            buildInputs = with pkgs;[
+            buildInputs = with pkgs; [
               (writeShellScriptBin "bazel" ''
                 exec ${bazelisk}/bin/bazelisk "$@"
               '')
               bazel-buildtools
+              rust-bin.stable.latest.default
             ];
           };
         }
