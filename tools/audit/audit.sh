79533b0d4aa47ffa76ddb7bf9c7491d63e0f8326
Kohei Watanabe <mypmc@vec.jp>
2024-04-06 16:23:03 +0900

    x/crates: bump h2 for RUSTSEC-2024-0332 (#156)
    Address RUSTSEC-2024-0332 advisory.
    
    https://rustsec.org/advisories/RUSTSEC-2024-0332

--- tools/audit/audit.sh
+++ tools/audit/audit.sh
@@ -4,9 +4,6 @@ set -euo pipefail -o posix
 CARGO_AUDIT_BIN="${CARGO_AUDIT_BIN:-cargo-audit}"
 
 LIB_CRATES_LOCK_FILE="${1:?Path to the crates lock file}"
-BIN_CRATES_LOCK_FILE="${2:?Path to the bin_crates lock file}"
-shift 2
+shift 1
 
-"${CARGO_AUDIT_BIN}" audit --file "${LIB_CRATES_LOCK_FILE}" --db ./advisory-db "$@" && \
-"${CARGO_AUDIT_BIN}" audit --file "${BIN_CRATES_LOCK_FILE}" --db ./advisory-db "$@" && \
-echo OK
+"${CARGO_AUDIT_BIN}" audit --file "${LIB_CRATES_LOCK_FILE}" --db ./advisory-db "$@"
