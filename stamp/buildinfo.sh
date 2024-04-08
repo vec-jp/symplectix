8268a65a6f6b9503b48fc5526921166b9774ddb2
Kohei Watanabe <mypmc@symplectik.com>
2024-04-08 16:51:29 +0900

    github_build_id

--- stamp/buildinfo.sh
+++ stamp/buildinfo.sh
@@ -17,18 +17,24 @@ git_status() {
 }
 echo "STABLE_GIT_STATUS $(git_status)"
 
-# Github Actions environment variables for stamping.
-# https://docs.github.com/en/actions/learn-github-actions/contexts#github-context
+# A unique build id for CI.
+github_build_id() {
+    # Github Actions environment variables for stamping.
+    # https://docs.github.com/en/actions/learn-github-actions/contexts#github-context
 
-# A unique number for each workflow run within a repository.
-# This number does not change if you re-run the workflow run.
-echo "STABLE_GITHUB_RUN_ID ${GITHUB_RUN_ID:=0}"
+    # A unique number for each workflow run within a repository.
+    # This number does not change if you re-run the workflow run.
+    local run_id="${GITHUB_RUN_ID:=0}"
 
-# A unique number for each run of a particular workflow in a repository.
-# This number begins at 1 for the workflow's first run, and increments with each new run.
-# This number does not change if you re-run the workflow run.
-echo "STABLE_GITHUB_RUN_NUMBER ${GITHUB_RUN_NUMBER:=0}"
+    # A unique number for each run of a particular workflow in a repository.
+    # This number begins at 1 for the workflow's first run, and increments with each new run.
+    # This number does not change if you re-run the workflow run.
+    local run_number="${GITHUB_RUN_NUMBER:=0}"
 
-# A unique number for each attempt of a particular workflow run in a repository.
-# This number begins at 1 for the workflow run's first attempt, and increments with each re-run.
-# echo "GITHUB_RUN_ATTEMPT ${GITHUB_RUN_ATTEMPT:=0}"
+    # A unique number for each attempt of a particular workflow run in a repository.
+    # This number begins at 1 for the workflow run's first attempt, and increments with each re-run.
+    local run_attempt="${GITHUB_RUN_ATTEMPT:=0}"
+
+    echo "${run_id}-${run_number}-${run_attempt}"
+}
+echo "STABLE_GITHUB_BUILD_ID $(github_build_id)"
