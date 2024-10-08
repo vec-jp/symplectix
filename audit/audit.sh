ca3b336c9c8341e2526b8d43efec5171d152a9f8
Kohei Watanabe <mypmc@vec.jp>
2024-10-08 10:51:46 +0900

    bazel: remove WORKSPACE.bazel (#176)
    - isolate `@crates`
    - remove //crates and //audit

--- audit/audit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail -o posix
-
-CARGO_AUDIT_BIN="${CARGO_AUDIT_BIN:-cargo-audit}"
-
-LIB_CRATES_LOCK_FILE="${1:?Path to the crates lock file}"
-shift 1
-
-"${CARGO_AUDIT_BIN}" audit --file "${LIB_CRATES_LOCK_FILE}" --db ./advisory-db "$@"
