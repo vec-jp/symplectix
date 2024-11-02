71dea2e25ae77b71dd8c8fadafe7dd3741d0fb90
Kohei Watanabe <mypmc@symplectix.com>
2024-11-02 16:54:35 +0900

    process/entrypoint: kill on drop (#77)

--- build/rules/fuzzing/cmd/run.rs
+++ build/rules/fuzzing/cmd/run.rs
@@ -1,14 +1,14 @@
-use entrypoint::ProcessWrapper;
+use entrypoint::Command;
 
 /// An entrypoint for fuzzing.
 #[derive(Clone, Debug, clap::Parser)]
 pub struct Run {
     #[command(flatten)]
-    process_wrapper: ProcessWrapper,
+    command: Command,
 }
 
 impl Run {
-    pub(crate) async fn run(&self) -> anyhow::Result<()> {
-        self.process_wrapper.run().await.map_err(anyhow::Error::from)
+    pub(crate) async fn run(self) -> anyhow::Result<()> {
+        self.command.run().await.map_err(anyhow::Error::from)
     }
 }
