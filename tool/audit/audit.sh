78da8c481fccee1367013c235e98ab491743f2ec
Kohei Watanabe <mypmc@symplectix.com>
2024-11-02 16:54:35 +0900

    ci: add a fuzzing workflow (#80)
    No tests yet.

--- tool/audit/audit.sh
+++ tool/audit/audit.sh
@@ -3,5 +3,6 @@ set -euo pipefail -o posix
 
 CARGO_AUDIT_BIN="${CARGO_AUDIT_BIN:-cargo-audit}"
 CARGO_LOCK_FILE="${1:?Path to the lock file to be audited}"
+shift 1
 
-"${CARGO_AUDIT_BIN}" audit --file "${CARGO_LOCK_FILE}"
+"${CARGO_AUDIT_BIN}" audit --file "${CARGO_LOCK_FILE}" --db ./advisory-db "$@"
