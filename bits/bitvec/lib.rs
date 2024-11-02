46965658e721a6545a8fddbedb30c4e599d0bdb6
Kohei Watanabe <mypmc@symplectix.com>
2024-11-02 16:54:34 +0900

    bits: Auxiliary data to help Rank and Select (#18)
    WIP

--- bits/bitvec/lib.rs
+++ bits/bitvec/lib.rs
@@ -1,3 +1,16 @@
+#![allow(dead_code)] // TODO: REMOVE
+#![allow(unused_imports)] // TODO: REMOVE
+
+mod l1l2;
+mod rank_aux;
+
+use bits::Bits;
+use l1l2::L1L2;
+// use rank_aux::{Buckets, Uninit};
+// use rank_aux::{Pop as L1L2Sum, Rho as L1L2Bit};
+
+pub use rank_aux::Rho;
+
 /// Calculates the minimum number of blocks to store `n` bits.
 const fn blocks(n: usize, b: usize) -> usize {
     n / b + (n % b > 0) as usize
@@ -30,3 +43,31 @@ pub fn empty<T: bits::Block>(n: usize) -> Vec<T> {
     use std::iter::from_fn;
     from_fn(|| Some(T::empty())).take(blocks(n, T::BITS)).collect::<Vec<T>>()
 }
+
+// // impl<T: Container, S> From<Imp<Box<[T]>, S>> for Imp<Vec<T>, S> {
+// //     #[inline]
+// //     fn from(imp: Imp<Box<[T]>, S>) -> Imp<Vec<T>, S> {
+// //         Imp { buckets: imp.buckets, samples: imp.samples, bit_vec: imp.bit_vec.into_vec() }
+// //     }
+// // }
+// // impl<T: Container, S> From<Imp<Vec<T>, S>> for Imp<Box<[T]>, S> {
+// //     #[inline]
+// //     fn from(imp: Imp<Vec<T>, S>) -> Imp<Box<[T]>, S> {
+// //         Imp { buckets: imp.buckets, samples: imp.samples, bit_vec: imp.bit_vec.into_boxed_slice() }
+// //     }
+// // }
+
+// // fn sbs_from_heaps<T: WordArray>(slice: &[Block<T>]) -> impl Iterator<Item = Option<&[T::Elem]>> {
+// //     assert!(Block::<T>::BITS % SUPER == 0 && SUPER <= 65536);
+// //     type BoxIter<'a, A> = Box<dyn Iterator<Item = Option<A>> + 'a>;
+
+// //     slice.iter().flat_map(move |heap| {
+// //         heap.as_slice().map_or_else(
+// //             || {
+// //                 use std::iter::repeat;
+// //                 Box::new(repeat(None).take(Block::<T>::BITS / SUPER)) as BoxIter<'_, &'_ [T::Elem]>
+// //             },
+// //             |b| Box::new(sbs_from_words(b)) as BoxIter<'_, &'_ [T::Elem]>,
+// //         )
+// //     })
+// // }
